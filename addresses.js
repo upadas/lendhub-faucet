b77a94cf8a33ff97777dc8bb28b3cf760b383508
Sasibhushan Upadrasta <supadrasta@gmail.com>
2023-04-15 17:40:14 +0530

    new deployment and new address

--- addresses.js
+++ addresses.js
@@ -6,8 +6,9 @@ const LINKAddress = "0x9FA894F65353cC2b4feCeA717B998a88a4641255";
 // const DAIAddress = "0xF2e2C8A502657c37ad4d37F2BefaBEb308315152";
 // const USDCAddress = "0x9603ed1F72C87d33d48a5e3A4A0915235BA46cFe";
 // const LINKAddress = "0x05332eF900c3B926ED013F66b64A85646Fd82092";
+const FaucetAddressOLD = "0x62C21bbd9dA59327Dee56F6a1312B569C64bB0e0";
+const FaucetAddress = "0xB5dD24d9d90F74249E82cf011843cd00b2BcFDbD";
 
-const FaucetAddress = "0x62C21bbd9dA59327Dee56F6a1312B569C64bB0e0";
 module.exports = {
   ETHAddress,
   DAIAddress,
--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import @openzeppelin/contracts/token/ERC20/IERC20.sol";
-import @openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import @openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Faucet is ReentrancyGuard{
     
@@ -12,75 +12,63 @@ contract Faucet is ReentrancyGuard{
     uint dailyETHThreshold = 0.5 ether;
     uint dailyTokenThreshold = 10;
     uint constant COOLDOWN_PERIOD = 1 days;
-    address sender = 0x4B40f99E93A8814be7fDe5F6AaFA5e9823E13728;
-    // IERC20 public daiToken;
-    // IERC20 public usdcToken;
-    // IERC20 publi1c linkToken;
-    event FaucetDrained (address to, uint amount);
+    IERC20 public daiToken;
+    IERC20 public usdcToken;
+    IERC20 public linkToken;
+
+    event Transfer (address to, uint amount);
     // Recipient Account => Timestamp
     mapping (address => uint) lastETHReceivedTimestamp;
     // IP Address => Timestamp
     mapping (string => uint) lastETHReceivedIPAddress;
 
-    // REcipient Account => Timestamp
-    mapping (address => uint) lastTokenReceivedTimestamp;
-    // IP Address => Timestamp
-    mapping (string => uint) lastTokenReceivedIPAddress;
-
-    IERC20 public daiToken = IERC20(0xfA36653d85B3A7D06Cd5d83e4d03667b36b44B7a);
-    IERC20 public usdcToken = IERC20(0xacEc0F50978654652d089E4e7016D9682a3b27CD);
-    IERC20 public linkToken = IERC20(0x9FA894F65353cC2b4feCeA717B998a88a4641255);
-    // 183.83.166.80
-    // DAI = '0xfA36653d85B3A7D06Cd5d83e4d03667b36b44B7a';
-    // USDC = '0xacEc0F50978654652d089E4e7016D9682a3b27CD';
-    // LINK = '0x9FA894F65353cC2b4feCeA717B998a88a4641255';
+    // Recipient Account => Token Address => Timestamp
+    mapping (address => mapping (address => uint)) lastTokenReceivedTimestamp;
+    // IP Address => Token Address => Timestamp
+    mapping (string => mapping (address => uint)) lastTokenReceivedIPAddress;
 
     modifier onlyOwner() {
-        require(msg.sender == faucetOwner, Not owner");
+        require(msg.sender == faucetOwner, "Not Owner");
         _;
     }
     
     receive() external payable {}
 
-    // constructor(address _daiAddress, address _usdcAddress, address _linkAddress) {
-    //     daiToken = IERC20(_daiAddress);
-    //     usdcToken = IERC20(_usdcAddress);
-    //     linkToken = IERC20(_linkAddress);
-    //     faucetOwner = msg.sender; 
-    // }
-
-    constructor() {
+    constructor(address _daiAddress, address _usdcAddress, address _linkAddress) {
+        daiToken = IERC20(_daiAddress);
+        usdcToken = IERC20(_usdcAddress);
+        linkToken = IERC20(_linkAddress);
         faucetOwner = msg.sender; 
     }
 
     function transferETH(string memory _ipAddress) external payable{
         require(msg.sender != address(0), "Invalid address");
         //This is to prevent Sybil attack
-        require(block.timestamp - lastETHReceivedIPAddress[_ipAddress] >= COOLDOWN_PERIOD, Request Not Allowed From This IP Address Yet");
+        require(block.timestamp - lastETHReceivedIPAddress[_ipAddress] >= COOLDOWN_PERIOD, "Request Not Allowed From This IP Address Yet");
         // Check for Cooldown period
-        require(block.timestamp - lastETHReceivedTimestamp[msg.sender] >= COOLDOWN_PERIOD, Request Not Allowed Yet");
-        (bool success,) = payable(msg.sender).call{value: dailyETHThreshold}(");
-        require(success, Failed to transfer ETH");
+        require(block.timestamp - lastETHReceivedTimestamp[msg.sender] >= COOLDOWN_PERIOD, "Request Not Allowed Yet");
+        (bool success,) = payable(msg.sender).call{value: dailyETHThreshold}("");
+        require(success, "Failed to transfer ETH");
         
         lastETHReceivedTimestamp[msg.sender] = block.timestamp;
         lastETHReceivedIPAddress[_ipAddress] = block.timestamp;
-        emit FaucetDrained (msg.sender, dailyETHThreshold);
+        emit Transfer (msg.sender, dailyETHThreshold);
     }
 
     function transferToken(address _token, string memory _ipAddress) external {
-        require(msg.sender != address(0), Invalid address");
-        require(IERC20(_token) == daiToken || IERC20(_token) == usdcToken || IERC20(_token) == linkToken, Token Not recognized");
-        require(block.timestamp - lastTokenReceivedIPAddress[_ipAddress] >= COOLDOWN_PERIOD, Request Not Allowed Yet");
-        require(block.timestamp - lastTokenReceivedTimestamp[msg.sender] >= COOLDOWN_PERIOD, Request Not Allowed Yet");
+        require(msg.sender != address(0), "Invalid address");
+        require(IERC20(_token) == daiToken || IERC20(_token) == usdcToken || IERC20(_token) == linkToken, "Token Not recognized");
+        require(block.timestamp - lastTokenReceivedIPAddress[_ipAddress][_token] >= COOLDOWN_PERIOD, "Request Not Allowed Yet");
+        require(block.timestamp - lastTokenReceivedTimestamp[msg.sender][_token] >= COOLDOWN_PERIOD, "Request Not Allowed Yet");
         
         SafeERC20.safeTransfer(IERC20(_token), msg.sender, dailyTokenThreshold);
         // should use mint instead of transfer
         // IERC20(_token).mint(msg.sender, dailyTokenThreshold);
         // daiToken.transfer(account1, numberToEthers(20000));
 
-        lastTokenReceivedTimestamp[msg.sender] = block.timestamp;
-        lastTokenReceivedIPAddress[_ipAddress] = block.timestamp;
-        emit FaucetDrained (msg.sender, dailyTokenThreshold);
+        lastTokenReceivedTimestamp[msg.sender][_token] = block.timestamp;
+        lastTokenReceivedIPAddress[_ipAddress][_token] = block.timestamp;
+        emit Transfer (msg.sender, dailyTokenThreshold);
     }
 
     function getDAITotalSupply() external view returns(uint) {
@@ -107,17 +95,8 @@ contract Faucet is ReentrancyGuard{
         return linkToken.balanceOf(msg.sender);
     }
 
-    // function getBalance(address _token, address _address) external view returns(uint){
-    //     return IERC20(_token).balanceOf(_address);
-    // }
-
     function withdraw(address _addr) public payable onlyOwner{
-        (bool success,) = _addr.call{value: address(this).balance}(");
-        require(success, Failed to withdraw ETH");
-    }
-
-    
+        (bool success,) = _addr.call{value: address(this).balance}("");
+        require(success, "Failed to withdraw ETH");
+    }   
 }
-
-
-// 0x62C21bbd9dA59327Dee56F6a1312B569C64bB0e0 - Faucet Address
